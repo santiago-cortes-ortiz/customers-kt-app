613755f6b17347cf700aa99cf55abde540284fd4
jeiss <jeissoncortes02@outlook.com>
2023-11-13 19:06:24 -0500

    add person

--- src/app/components/person-add/person-add.component.ts
+++ src/app/components/person-add/person-add.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Person } from '../../models/person.model';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { PersonService } from 'src/app/services/person.service';
 
 @Component({
   selector: 'app-person-add',
@@ -8,14 +9,45 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./person-add.component.css']
 })
 export class PersonAddComponent {
-  personForm = new FormGroup({
-    name: new FormControl(''),
-    lastName: new FormControl(''),
-    age: new FormControl(''),
-    birthdate: new FormControl(''),
-  });
+
+  personForm!: FormGroup;
   
+  constructor(private _personService: PersonService) {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.personForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      age: new FormControl('', Validators.required),
+      birthdate: new FormControl('', Validators.required),
+    });
+  }
+
   addPerson() {
-    console.log(this.personForm.value);
+    if (this.personForm.valid) {
+      const formValues = this.personForm.getRawValue();
+      const personToSave: Person = {
+        name: formValues.name!,
+        lastName: formValues.lastName!,
+        age: Number(formValues.age),
+        birthdate: formValues.birthdate!,
+      }
+      this._personService.addPerson(personToSave)
+      .subscribe({
+        next: (person) => console.log(person),
+        error: (err) => console.log(err),
+        complete: () => console.log('Completed')
+      }); 
+    } else {
+      this.openModalError();
+    }
+  }
+
+  openModalError() {
+    const modalContent = "El formulario no es válido. Por favor, revisa los campos e inténtalo de nuevo.";
+    window.alert(modalContent);
   }
 }
+
